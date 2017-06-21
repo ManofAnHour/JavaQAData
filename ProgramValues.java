6d2a17f791551d2e6b25de69cd9d7dc1abc1dec0
kconners <kconners@brierley.com>
2017-06-21 11:43:40 -0500

    Starting
    This is a start

---
+++ ProgramValues.java
@@ -0,0 +1,295 @@
+package QaTestingObjects;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+//import org.json.*;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.*;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.client.entity.*;
+import org.json.simple.*;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import com.google.gson.Gson;
+
+
+
+public class ProgramValues {
+	
+	public class RunItems
+	{
+        public String Client_Long = "";
+        public String Org_BeingTested = "";
+        public String Env_Shrt = "";
+        public String username = "";
+        public String Orgs_Env_Shrt = "";
+        public String OrgsEnv_LongName = "";
+        public String Release = "";
+        public String Build = "";
+        public String Application = "";
+        public String Itereation = "";
+        public String LWVersion = "";
+        public String LWBuild = "";
+        public UUID LW_Version = new UUID(0L, 0L);
+        public UUID LW_Build =  new UUID(0L, 0L);
+        
+	}
+    public class urlRoot
+    {
+		public url urls;
+    }
+    public class url
+    {
+        public int Status;
+
+        public String application = "";
+        public UUID application_idnumber = new UUID(0L, 0L);
+        public String ap_srtname = "";
+        public UUID client_idnumber = new UUID(0L, 0L);
+        public String description = "";
+        public UUID env_idnumber = new UUID(0L, 0L);
+        public String fullurl = "";
+        public String name = "";
+        public UUID program_idnumber = new UUID(0L, 0L);
+        public UUID row_idnumber = new UUID(0L, 0L);
+    }
+	
+    public class runnerdetailsRoot
+    {
+		public ScriptRunner runnerdetails;
+    }
+    
+    public class ScriptRunner
+    {
+        public String TestSetTitle = "";
+        public String UserName = "" ;
+    }
+    
+	
+	public class URLForUse{
+		public List<org.apache.http.NameValuePair> listofparam;
+	}
+	public static RunItems GetTestingPreferences(String username, String Application) throws IllegalStateException, IOException
+	{
+		ProgramValues PV = new ProgramValues();
+		RunItems RI = PV.new RunItems(); 
+		
+		NameValuePair[] params = new BasicNameValuePair[] {
+		        new BasicNameValuePair("loggedInas", username),
+		        new BasicNameValuePair("application", Application),
+		};
+		
+		HttpGet getRequest = new HttpGet();
+		try {
+			getRequest = new HttpGet(BuildURL("/api/UserTestingPrefForRunItem", params));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		
+		HttpClient httpClient =  HttpClientBuilder.create().build();;
+		HttpResponse response = httpClient.execute(getRequest);
+
+
+		BufferedReader br = new BufferedReader(
+                         new InputStreamReader((response.getEntity().getContent())));
+		
+		
+		Gson gson = new Gson();
+
+		RI = gson.fromJson(br.readLine(), RunItems.class);
+		RI.Application = Application;
+
+		return RI;
+	}
+	public String getURL(RunItems RI) throws IllegalStateException, ClientProtocolException, IOException
+	{
+		String URL = "";
+		String eenv = "";
+
+		if(RI.Orgs_Env_Shrt == null || RI.Orgs_Env_Shrt == "")
+		{
+			eenv = RI.Env_Shrt;
+		}
+		else
+		{
+            if (RI.Orgs_Env_Shrt == RI.Env_Shrt) eenv = RI.Env_Shrt;
+            else if (RI.Orgs_Env_Shrt != RI.Env_Shrt) eenv = RI.Orgs_Env_Shrt;
+		}
+         
+         ProgramValues PV = new ProgramValues();
+         url uRL = PV.new url();
+
+         NameValuePair[] params = new BasicNameValuePair[] {
+ 		        new BasicNameValuePair("client", RI.Org_BeingTested),
+ 		        new BasicNameValuePair("program", "All"),
+ 		        new BasicNameValuePair("environment", RI.Orgs_Env_Shrt),
+ 		        new BasicNameValuePair("application", RI.Application),
+ 		};
+ 		
+ 		HttpGet getRequest = new HttpGet();
+ 		try {
+ 			getRequest = new HttpGet(BuildURL("api/URL", params));
+ 		} catch (ParseException e) {
+ 			e.printStackTrace();
+ 		}
+ 	
+ 		
+ 		HttpClient httpClient =  HttpClientBuilder.create().build();;
+ 		HttpResponse response = httpClient.execute(getRequest);
+
+ 		
+ 		BufferedReader br = new BufferedReader(
+                          new InputStreamReader((response.getEntity().getContent())));
+		//System.out.println("Output from Server ...."+br.readLine()+" \n");
+ 		Gson gson = new Gson();
+
+ 		urlRoot urlroot = PV.new urlRoot();
+ 		
+ 		urlroot = gson.fromJson(br.readLine(), urlRoot.class);
+ 		
+ 		uRL = urlroot.urls;
+         
+        URL = uRL.fullurl;
+         
+		return URL;
+	}
+	public static void TryRest() throws IllegalStateException, IOException
+	{
+		HttpClient httpClient =  HttpClientBuilder.create().build();;
+		URI uri = null;
+		
+			try {
+				uri = new URIBuilder()
+				        .setScheme("http")
+				        .setHost(conFIG.URL)
+				        .setPath("/api/QAComplete/LastUpdateDate")
+				        .setParameter("Client_IDNUM", "4a8f16cd-3874-4237-a601-50468615949a")
+				        .setParameter("Type", "Link")
+				        .build();
+			} catch (URISyntaxException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		
+		
+		HttpGet getRequest = new HttpGet(uri);
+
+		HttpResponse response = httpClient.execute(getRequest);
+
+
+
+		BufferedReader br = new BufferedReader(
+                         new InputStreamReader((response.getEntity().getContent())));
+
+		String output;
+		//System.out.println("Output from Server .... \n");
+		System.out.println("Output from Server ...."+br.readLine()+" \n");
+		//while ((output = br.readLine()) != null) {System.out.println(output);		}
+
+		httpClient.getConnectionManager().shutdown();
+		
+	}
+	public ScriptRunner GetScriptRunnerInfo(String ClientName, String ScriptName) throws ClientProtocolException, IOException
+	{
+		  	 ProgramValues PV = new ProgramValues();
+	         ScriptRunner sr = PV.new ScriptRunner();
+		
+
+	         NameValuePair[] params = new BasicNameValuePair[] {
+	 		        new BasicNameValuePair("Client", ClientName),
+	 		        new BasicNameValuePair("ScriptName", ScriptName)
+	 		        
+	 		};
+	 		
+	 		HttpGet getRequest = new HttpGet();
+	 		try {
+	 			getRequest = new HttpGet(BuildURL("api/ScriptRunner", params));
+	 		} catch (ParseException e) {
+	 			e.printStackTrace();
+	 		}
+	 	
+	 		
+	 		HttpClient httpClient =  HttpClientBuilder.create().build();;
+	 		HttpResponse response = httpClient.execute(getRequest);
+
+	 		
+	 		BufferedReader br = new BufferedReader(
+	                          new InputStreamReader((response.getEntity().getContent())));
+			//System.out.println("Output from Server ...."+br.readLine()+" \n");
+	 		Gson gson = new Gson();
+
+	 		runnerdetailsRoot runnerdetroot = PV.new runnerdetailsRoot();
+	 		
+	 		runnerdetroot = gson.fromJson(br.readLine(), runnerdetailsRoot.class);
+	 		
+	 		sr = runnerdetroot.runnerdetails;
+	         
+	         return sr;
+	}
+	public static URI BuildURL(String Path, NameValuePair[] params) throws ParseException
+	{		
+	URI uri = null;
+	
+	List<org.apache.http.NameValuePair> ListOFParam = new ArrayList<org.apache.http.NameValuePair>();
+	
+	if(params.length >= 1)
+	{
+		 for(org.apache.http.NameValuePair I : params)
+		 {
+			 ListOFParam.add(new BasicNameValuePair(I.getName(), I.getValue()));
+		 }
+	}
+	
+	try {
+		uri = new URIBuilder()
+		        .setScheme("http")
+		        .setHost(conFIG.URL)
+		        .setPath(Path)
+		        
+		        .setParameters(ListOFParam)
+		        .build();
+	} catch (URISyntaxException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+	return uri;
+
+	}
+	
+	
+	
+	public static void main(String[] args) throws IllegalStateException, IOException {
+		
+		ProgramValues PV = new ProgramValues();
+		RunItems RI = PV.new RunItems(); 
+		ScriptRunner SR = PV.new ScriptRunner();
+		
+		SR = PV.GetScriptRunnerInfo("Wendys","GetRewardCatalog");
+		RI =  GetTestingPreferences(SR.UserName, "API");
+		
+		String URL = PV.getURL(RI);
+		System.out.println("Output from Server ...."+URL+" \n");
+		
+	}
+}
